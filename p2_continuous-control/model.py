e718bbed1304d00172408e9f1acdaa482b78c34d
Amit Nativ <amitnativ@gmail.com>
2024-01-31 10:24:41 +0000

    finshed project #2

--- p2_continuous-control/model.py
+++ p2_continuous-control/model.py
@@ -1,83 +1,74 @@
 import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
 
 class Actor(nn.Module):
-    """ Actor Policy Model """
+    """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed):
-        """ Initialize parameters and build model
+    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+        """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
-            seed(int): Random seed
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
         """
-
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, 400)
-        self.fc2 = nn.Linear(400, 300)
-        self.fc3 = nn.Linear(300, action_size)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
         self.reset_parameters()
 
     def reset_parameters(self):
-        nn.init.xavier_uniform_(self.fc1)
-        nn.init.xavier_uniform_(self.fc2)
-        nn.init.xavier_uniform_(self.fc3)
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
-        """
-            Build an actor (policy) network that maps states -> actions
-            The output actions are control inputs to the agent, and need to be 
-            in the range of [-1, 1]
-        """
-        
+        """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        actions = torch.tanh(x)
-        return actions
-    
-class Critic(nn.Module):
-    """ Critic Value Model """
+        return F.tanh(self.fc3(x))
 
-    def __init__(self, state_size, action_size, seed):
-        """ Initialize parameters and build model
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+        """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
-            seed(int): Random seed
-
-            The input to the network is the state, and the action chosen by the Actor.
-            The network outputs a single value, Q(s,a)
-
-            The actions are not included until the 2nd hidden layer of the network.
+            seed (int): Random seed
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer
         """
-
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, 400)
-        self.fc2 = nn.Linear(400+action_size, 300)
-        self.fc3 = nn.Linear(300, 1)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
-        nn.init.xavier_uniform_(self.fc1)
-        nn.init.xavier_uniform_(self.fc2)
-        nn.init.xavier_uniform_(self.fc3)
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
-        """
-            Build a critic (value) network that maps (state, action) pairs -> Q-values
-        """
-        
-        xs = F.relu(self.fc1(state))
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        xs = F.relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-        
+        return self.fc3(x)
